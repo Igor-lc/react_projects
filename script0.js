1d60ac08ff3baa84d0a566b7bc3106efd9cb1e05
Igor <lc11@ukr.net>
2024-04-13 09:07:08 +0300

    first commit

---
+++ script0.js
@@ -0,0 +1,13 @@
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+
+console.log(obj);
+console.log(obj["age"]);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple,bmp', {}, []]
+console.log(arr[0])
